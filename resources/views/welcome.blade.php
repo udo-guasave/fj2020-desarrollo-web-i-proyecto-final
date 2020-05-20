99700039d16b19611f42a8ce0c22f538d2c7f535
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2020-05-20 11:43:25 -0600

    Clase 18 - Reto de clase 17
    - Cambio de nombre de la aplicación desde la vista directamente

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    MyFirst&LastLaravelAppOfSemester
                 </div>
 
                 <div class="links">
